f05983697a10c08c4311696cd21939a31f0ecaad
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-22 20:08:51 +0700

    update forget.py

--- forget.py
+++ forget.py
@@ -513,7 +513,7 @@ def userset():
     else:print(f"{P}[•] Pilihan salah ");exit()
 
 #####LOGIN HASIL
-class Eksekusi(started):
+class log_hasil(main):
 	
 	def cek_opsi(self):
 		global aman,cpsalah
